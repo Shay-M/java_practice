6df381afeb4f0830046f63e22b7aaa90ebe02db9
shay <22634623+Shay-M@users.noreply.github.com>
2022-06-17 11:47:34 +0300

    Delete java-Areal directory

--- java-Areal/src/Tests2022/A97105/ICanFly.java
+++
@@ -1,5 +0,0 @@
-package Tests2022.A97105;
-
-public interface ICanFly {
-    void fly();
-}
--- java-Areal/src/Tests2022/Mgan/Ex8b.java
+++
@@ -1,8 +0,0 @@
-package Tests2022.Mgan;
-
-public class Ex8b {
-
-    public static void main(String[] args) {
-
-    }
-}
