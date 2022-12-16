ce821080e60a7ac9028cdfc1a963934727825d56
“ShayM” <shay1mualem@gmail.com>
2022-12-16 12:37:32 +0200

    Day 10 update

--- Experis/Day9/src/Reversesllistinplace/Main.java
+++ Experis/Day9/src/Reversesllistinplace/Main.java
@@ -4,6 +4,7 @@ package Reversesllistinplace;
 //- Calculate the Complexity of the function if the function if the List passed to it as an ArrayList.
 //- Calculate the Complexity of the function if the function if the List passed to it as a LinkedList.
 //- Is it possible to implement the function with Complexity O(N) for BOTH types of List? (Note that List supports a listIterator() method)
+// Example: the list ["a", "b", "c"] will be changed into ["c", "b", "a"]
 
 
 import java.util.*;
@@ -15,33 +16,31 @@ public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getCanonicalName());
 
     public static void main(String[] args) {
-
         final List<String> listOfLetters = new LinkedList<String>();
         listOfLetters.add("a");
         listOfLetters.add("b");
         listOfLetters.add("c");
 
-        final List<String> arrayOfLetters = new ArrayList<String>();
-        arrayOfLetters.add("a");
-        arrayOfLetters.add("b");
-        arrayOfLetters.add("c");
-
-        ReversesList(listOfLetters);
-        logger.log(Level.INFO, "LinkedList. " + listOfLetters); // N * (1 + 1) \/ N * (1 + N)
-
-        ReversesList(arrayOfLetters);
-        logger.log(Level.INFO, "ArrayList. " + arrayOfLetters); // N * (N + 1)
-
+        logger.log(Level.INFO, "Before LinkedList " + listOfLetters);
+        ReversesListWithListIterator(listOfLetters);
+        logger.log(Level.INFO, "After LinkedList. " + listOfLetters);
     }
 
-    //Example: the list ["a", "b", "c"] will be changed into ["c", "b", "a"]
-    private static void ReversesList(List listOfLetters) {
-        final int sizeList = listOfLetters.size();
-
+    private static void ReversesList(List listOfLetters) { // O()
+        final int sizeList = listOfLetters.size(); // LinkedList: 1 | ArrayList: 1
         for (int i = 0; i < sizeList; i++) {
-            listOfLetters.add(i, listOfLetters.get(listOfLetters.size() - 1));
-            listOfLetters.remove(listOfLetters.size() - 1);
+            listOfLetters.add(i, listOfLetters.get(listOfLetters.size() - 1)); // LinkedList: add: 1 + get: N | ArrayList: add: N + get: 1
+            listOfLetters.remove(listOfLetters.size() - 1); // LinkedList: remove: N | ArrayList: remove: N
         }
-
     }
+    private static void ReversesListWithListIterator(List listOfLetters) { // O(N)
+        final ListIterator<String> iteratorListToReturnFromStart = listOfLetters.listIterator();
+
+        final int listSize = listOfLetters.size();
+        for (int index = 0; index < listSize / 2; ++index) { // N/2 *
+
+        }
+    }
+
 }
+
