bc3ca0945fc306b00b9dd54dbd5846ec5c453817
“ShayM” <shay1mualem@gmail.com>
2022-12-25 18:34:17 +0200

    update

--- Experis/Day16/Todo/src/tasks/TasksBundle.java
+++ Experis/Day16/Todo/src/tasks/TasksBundle.java
@@ -6,34 +6,34 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public final class TasksBundle implements Iterable<Entry<Task, MutableState>> {
-	private final Map<Task, MutableState> m_tasks = new HashMap<>();
-	
-	public final void add(final Task task) {
-		if (m_tasks.containsKey(task)) {
-			throw new TaskAlreadyExistsException(task);
-		}
-		final int oldNumTasks = m_tasks.size();
-		m_tasks.put(task, new MutableState());
-		assert (m_tasks.size() == oldNumTasks + 1);
-	}
-	
-	public final Iterator<Entry<Task, MutableState>> iterator() {
-		return m_tasks.entrySet().iterator();
-	}
+    private final Map<Task, MutableState> m_tasks = new HashMap<>();
 
-	public final boolean isEmpty() {
-		return m_tasks.isEmpty();
-	}
+    public final void add(final Task task) {
+        if (m_tasks.containsKey(task)) {
+            throw new TaskAlreadyExistsException(task);
+        }
+        final int oldNumTasks = m_tasks.size();
+        m_tasks.put(task, new MutableState());
+        assert (m_tasks.size() == oldNumTasks + 1);
+    }
 
-	public final int size() {
-		return m_tasks.size();
-	}
+    public final Iterator<Entry<Task, MutableState>> iterator() {
+        return m_tasks.entrySet().iterator();
+    }
 
-	public final MutableState getState(Task task) {
-		if (!m_tasks.containsKey(task)) {
-			throw new IllegalArgumentException("task not found: " + task.toString());
-		}
-		
-		return m_tasks.get(task);
-	}
+    public final boolean isEmpty() {
+        return m_tasks.isEmpty();
+    }
+
+    public final int size() {
+        return m_tasks.size();
+    }
+
+    public final MutableState getState(Task task) {
+        if (!m_tasks.containsKey(task)) {
+            throw new IllegalArgumentException("task not found: " + task.toString());
+        }
+
+        return m_tasks.get(task);
+    }
 }
