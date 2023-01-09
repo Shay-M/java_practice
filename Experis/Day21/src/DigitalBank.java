4c84c94220a2ac4f59b9f0c2d316647580a5efc6
“ShayM” <shay1mualem@gmail.com>
2023-01-09 16:09:57 +0200

    add web

--- Experis/Day21/src/DigitalBank.java
+++ Experis/Day21/src/DigitalBank.java
@@ -1,7 +1,10 @@
 import db.DataBaseSqlPassword;
 import db.SqlDBConnection;
 import db.CustomerDAOImpl;
+import super_simple_web_server.Status;
+import super_simple_web_server.SuperSimpleWebServer;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,12 +14,12 @@ import java.util.logging.Logger;
 
 import static java.util.logging.Logger.getLogger;
 
-public class Main {
-    private static final Logger m_logger = getLogger(Main.class.getCanonicalName());
-
-
-
-
+public class DigitalBank {
+    private static final Logger m_logger = getLogger(DigitalBank.class.getCanonicalName());
+    // private static final List<Map.Entry<String, WebAction>> PARAMETERIZED_PAGES = new ArrayList<>();
+    //private static final List<Map.Entry<String, WebAction>> EXACT_PAGES = new ArrayList<>();
+    private static final int PORT = 9797;
+    // sql
     private static final String ROOT_SQL_URL = "jdbc:mysql://localhost/";
     private final static String SQL_USER = "root";
     private final static String DB_NAME = "SecondDigitalBankTest";
@@ -31,13 +34,30 @@ public class Main {
                 CustomerDAOImpl workerDao = new CustomerDAOImpl(sqlDBConnection);
 
                 // final String name = workerDao.getCustomer(1000);
-                m_logger.log(Level.INFO, "list: " + workerDao.getContact("yosi").get(0).getName());
-
+                // m_logger.log(Level.INFO, "list: " + workerDao.getContact("yosi").get(0).getName());
+                final DigitalBank server = new DigitalBank();
+                server.serverLoop(workerDao);
             }
             catch (SQLException ex) {
                 m_logger.log(Level.WARNING, "" + ex.getMessage());
             }
+
         }
 
+    }
+
+    private void serverLoop(CustomerDAOImpl workerDao) {
+        try (SuperSimpleWebServer server = new SuperSimpleWebServer(PORT, m_logger)) {
+            while (true) {
+                try (SuperSimpleWebServer.Request request = server.waitForRequest()) {
+                    request.getWriter(Status.OK)
+                            .write("<H1>DigitalBank</H1>" +  workerDao.getContact("yosi").get(0).toString());
+
+                }
+            }
+        }
+        catch (IOException ex) {
+            m_logger.log(Level.SEVERE, "IOException " + ex.getMessage());
+        }
     }
 }
---
+++ Experis/Day21/src/webaction/WebAction.java
@@ -0,0 +1,7 @@
+package webaction;
+
+import super_simple_web_server.SuperSimpleWebServer.Request;
+
+public interface WebAction {
+	// String doAction(final Request request, final String untrust_remainingUriParams, final TasksBundle tasks);
+}
